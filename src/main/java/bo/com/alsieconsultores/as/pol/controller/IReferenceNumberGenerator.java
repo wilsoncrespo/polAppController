f9b16d991c521f20beda00623899c5a48aabcdb4
wilson crespo <wilsoncrespo@gmail.com>
2019-04-26 13:15:35 -0400

    Initial commit of the POL Application Controller project

---
+++ src/main/java/bo/com/alsieconsultores/as/pol/controller/IReferenceNumberGenerator.java
@@ -0,0 +1,5 @@
+package bo.com.alsieconsultores.as.pol.controller;
+
+public interface IReferenceNumberGenerator {
+    String getReferenceNumber();
+}
---
+++ src/main/java/bo/com/alsieconsultores/as/pol/controller/POLApplicationController.java
@@ -0,0 +1,27 @@
+package bo.com.alsieconsultores.as.pol.controller;
+
+import bo.com.alsieconsultores.as.pol.model.POLApplication;
+import bo.com.alsieconsultores.as.pol.model.POLApplicationPersonalInfo;
+import bo.com.alsieconsultores.as.pol.model.POLApplicationResume;
+
+public class POLApplicationController {
+    private POLApplication polApplication;
+    private String referenceNumber;
+    private ReferenceNumberGenerator referenceNumberGenerator = new ReferenceNumberGenerator();
+
+    public POLApplicationController(POLApplication polApplication) {
+        this.referenceNumber = referenceNumberGenerator.getReferenceNumber();
+        this.polApplication = polApplication;
+        setReferenceNumber(this.polApplication.getPolApplicationPersonalInfo(), this.polApplication.getPolApplicationResume(), this.referenceNumber);
+    }
+
+    public void setReferenceNumber(POLApplicationPersonalInfo polApplicationPersonalInfo, POLApplicationResume polApplicationResume, String referenceNumber) {
+        polApplicationPersonalInfo.setReferenceNumber(referenceNumber);
+        polApplicationResume.setReferenceNumber(referenceNumber);
+    }
+
+    public void savePOLApplication() {
+        //TODO call to the polApplicationProducer instance with the polApplication instance loaded and ready to be saved
+
+    }
+}
